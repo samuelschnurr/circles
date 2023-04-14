8e9184a7e84941eda88d40cdebf8f20d58b1f156
Samuel Schnurr <ssn@objektkultur.de>
2023-04-14 20:53:52 +0200

    Add property id to WeatherForecast

--- Io.Schnurr.Circles.Shared/WeatherForecast.cs
+++ Io.Schnurr.Circles.Shared/WeatherForecast.cs
@@ -1,13 +1,15 @@
 namespace Io.Schnurr.Circles.Shared
 {
-	public class WeatherForecast
-	{
-		public DateOnly Date { get; set; }
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
 
-		public int TemperatureC { get; set; }
+        public DateOnly Date { get; set; }
 
-		public string? Summary { get; set; }
+        public int TemperatureC { get; set; }
 
-		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-	}
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
 }
