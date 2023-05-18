f4051e74f384b6ab19d5a3d13bf03e3411770cb9
Samuel Schnurr <ssn@objektkultur.de>
2023-05-18 16:16:20 +0200

    Setup MudBlazor

--- Io.Schnurr.Circles.App/Program.cs
+++ Io.Schnurr.Circles.App/Program.cs
@@ -1,11 +1,13 @@
 using Io.Schnurr.Circles.App;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiHttpsPath"] ?? throw new Exception("Api Path not found")) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
