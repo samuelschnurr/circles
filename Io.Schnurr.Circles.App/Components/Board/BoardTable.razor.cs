8476ee4804a514eac701d47ab30f8d1dd9f0b4f2
Samuel Schnurr <ssn@objektkultur.de>
2023-05-21 19:24:32 +0200

    Update BoardTable with weatherforecast fields

--- Io.Schnurr.Circles.App/Components/Board/BoardTable.razor.cs
+++ Io.Schnurr.Circles.App/Components/Board/BoardTable.razor.cs
@@ -9,31 +9,44 @@ public partial class BoardTable
 {
     [Inject]
     private HttpClient HttpClient { get; set; } = default!;
-    private IEnumerable<WeatherForecast> pagedData;
-    private MudTable<WeatherForecast> table;
+    private IEnumerable<WeatherForecast>? pagedData;
+    private MudTable<WeatherForecast>? table;
     private int totalItems;
-    private string searchString = null;
+    private string? searchString;
 
-    /// <summary>
-    /// Here we simulate getting the paged, filtered and ordered data from the server
-    /// </summary>
     private async Task<TableData<WeatherForecast>> ServerReload(TableState state)
     {
         IEnumerable<WeatherForecast>? data = await HttpClient.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
-        await Task.Delay(300);
+
         data = data!.Where(element =>
         {
-            if (string.IsNullOrWhiteSpace(searchString))
-                return true;
-            if (element.Summary!.Contains(searchString, StringComparison.OrdinalIgnoreCase))
-                return true;
-            return false;
+            bool result = false;
+
+            result = string.IsNullOrWhiteSpace(searchString)
+                || element.Id.ToString().Contains(searchString, StringComparison.OrdinalIgnoreCase)
+                || element.Date.ToShortDateString().Contains(searchString, StringComparison.OrdinalIgnoreCase)
+                || element.TemperatureC.ToString().Contains(searchString, StringComparison.OrdinalIgnoreCase)
+                || element.TemperatureF.ToString().Contains(searchString, StringComparison.OrdinalIgnoreCase)
+                || element.Summary!.Contains(searchString, StringComparison.OrdinalIgnoreCase);
+
+            return result;
         }).ToArray();
+
         totalItems = data.Count();
+
         switch (state.SortLabel)
         {
             case "id_field":
                 data = data.OrderByDirection(state.SortDirection, o => o.Id);
+                break;
+            case "date_field":
+                data = data.OrderByDirection(state.SortDirection, o => o.Date);
+                break;
+            case "temperatureC_field":
+                data = data.OrderByDirection(state.SortDirection, o => o.TemperatureC);
+                break;
+            case "temperatureF_field":
+                data = data.OrderByDirection(state.SortDirection, o => o.TemperatureF);
                 break;
             case "summary_field":
                 data = data.OrderByDirection(state.SortDirection, o => o.Summary);
@@ -41,6 +54,7 @@ public partial class BoardTable
         }
 
         pagedData = data.Skip(state.Page * state.PageSize).Take(state.PageSize).ToArray();
+
         return new TableData<WeatherForecast>() { TotalItems = totalItems, Items = pagedData };
     }
 
