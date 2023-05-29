85598138f61d671460c8e83334517748fab3f445
Samuel Schnurr <ssn@objektkultur.de>
2023-05-29 20:25:10 +0200

    Refactored Index and implement progressbar

--- Io.Schnurr.Circles.App/Pages/Index.razor.cs
+++ Io.Schnurr.Circles.App/Pages/Index.razor.cs
@@ -1,17 +1,23 @@
-﻿namespace Io.Schnurr.Circles.App.Pages;
+﻿using Io.Schnurr.Circles.App.Interfaces;
 
-public partial class Index
+namespace Io.Schnurr.Circles.App.Pages;
+
+public partial class Index : ILoadingComponent
 {
-    private bool IsTileView { get; set; }
+    private bool? IsTileView { get; set; }
+
+    public bool IsLoading() => IsTileView == null;
 
     protected override async Task OnInitializedAsync()
     {
-        IsTileView = await LocalStorage.GetItemAsync<bool>(nameof(IsTileView));
+        IsTileView = await LocalStorage.GetItemAsync<bool?>(nameof(IsTileView));
     }
 
-    private async Task HandleIsTileViewChanged(bool value)
+    private async Task HandleIsTileViewChanged(bool? value)
     {
         IsTileView = value;
         await LocalStorage.SetItemAsync(nameof(IsTileView), value);
     }
+
+    private bool ShowTileView() => IsTileView!.Value;
 }
