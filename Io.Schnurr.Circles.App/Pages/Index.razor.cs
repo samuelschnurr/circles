b8af00af8f2fecfacb3bba3dc6f11a4a8b8fc0bb
Samuel Schnurr <ssn@objektkultur.de>
2023-05-29 15:47:53 +0200

    First state implementation of IsTileView

--- Io.Schnurr.Circles.App/Pages/Index.razor.cs
+++ Io.Schnurr.Circles.App/Pages/Index.razor.cs
@@ -2,5 +2,16 @@
 
 public partial class Index
 {
-    private bool isTileView;
+    private bool IsTileView { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsTileView = await LocalStorage.GetItemAsync<bool>(nameof(IsTileView));
+    }
+
+    private async Task HandleIsTileViewChanged(bool value)
+    {
+        IsTileView = value;
+        await LocalStorage.SetItemAsync(nameof(IsTileView), value);
+    }
 }
