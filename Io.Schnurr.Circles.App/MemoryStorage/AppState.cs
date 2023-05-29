4ab9f083df9e881ad6e17f64ecfff14108fa4ede
Samuel Schnurr <ssn@objektkultur.de>
2023-05-29 20:31:48 +0200

    Cleaneup AppState because not used anymore

--- Io.Schnurr.Circles.App/MemoryStorage/AppState.cs
+++ Io.Schnurr.Circles.App/MemoryStorage/AppState.cs
@@ -1,21 +1,4 @@
 ﻿namespace Io.Schnurr.Circles.App.MemoryStorage;
 
 public class AppState
-{
-    public bool isLoading;
-
-    public bool IsLoading
-    {
-        get => isLoading;
-        set
-        {
-            isLoading = value;
-            NotifyStateChanged();
-        }
-    }
-
-    public event Action? OnChange;
-
-    private void NotifyStateChanged() => OnChange?.Invoke();
-
-}
+{ }
