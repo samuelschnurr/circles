e8334e2601e7368eaa89b3eb8cfed094b9a9d3bf
Samuel Schnurr <ssn@objektkultur.de>
2023-05-29 16:56:21 +0200

    Add Utils and StartupHelper

--- Io.Schnurr.Circles.App/Program.cs
+++ Io.Schnurr.Circles.App/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using Io.Schnurr.Circles.App;
+using Io.Schnurr.Circles.App.Utils;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -8,8 +9,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiHttpsPath"] ?? throw new Exception("Api Path not found")) });
+builder.AddApiConfiguration();
+
 builder.Services.AddMudServices();
+
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMemoryStorage();
 
 await builder.Build().RunAsync();
---
+++ Io.Schnurr.Circles.App/Utils/StartupHelper.cs
@@ -0,0 +1,22 @@
+﻿using Io.Schnurr.Circles.App.States;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Io.Schnurr.Circles.App.Utils;
+
+internal static class StartupHelper
+{
+    internal static void AddApiConfiguration(this WebAssemblyHostBuilder builder)
+    {
+        var baseAddress = builder.Configuration["ApiHttpsPath"] ?? throw new Exception("Api Path not found");
+
+        builder.Services.AddScoped(sp => new HttpClient
+        {
+            BaseAddress = new Uri(baseAddress)
+        });
+    }
+
+    internal static void AddMemoryStorage(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<AppState>();
+    }
+}
