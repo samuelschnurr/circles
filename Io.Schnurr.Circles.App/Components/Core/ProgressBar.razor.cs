1320e2c3fde99104552ce25ae628c6a3fbea603c
Samuel Schnurr <ssn@objektkultur.de>
2023-06-02 20:43:46 +0200

    Chang linear progress to circular and center

---
+++ Io.Schnurr.Circles.App/Components/Core/ProgressBar.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Io.Schnurr.Circles.App.Components.Core;
+
+public partial class ProgressBar
+{
+    private string GetCenterCircularStyle()
+    {
+        string centerCircularStyle = "height: calc(100vh - 176px);";
+
+        if (false)
+        {
+            centerCircularStyle += "width: calc(100% var(--mud-drawer-width,var(--mud-drawer-width-left)));";
+        }
+
+        return centerCircularStyle;
+    }
+}
