69d5359a6c4f2783426edf1558c2d4aa1c579223
Samuel Schnurr <ssn@objektkultur.de>
2023-06-03 14:21:56 +0200

    Add LoadStates to Main

---
+++ Io.Schnurr.Circles.App/Components/Base/MainLayout.razor.cs
@@ -0,0 +1,19 @@
+﻿using Io.Schnurr.Circles.App.Store.App;
+
+namespace Io.Schnurr.Circles.App.Components.Base;
+
+public partial class MainLayout
+{
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            LoadStates();
+        }
+    }
+
+    private void LoadStates()
+    {
+        Dispatcher.Dispatch(new AppStateLoadAction());
+    }
+}
