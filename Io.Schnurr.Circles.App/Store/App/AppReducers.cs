35d19df004032046693737459f23976e6745bd68
Samuel Schnurr <ssn@objektkultur.de>
2023-06-11 16:50:32 +0200

    Fix build error

--- Io.Schnurr.Circles.App/Store/App/AppReducers.cs
+++ Io.Schnurr.Circles.App/Store/App/AppReducers.cs
@@ -5,7 +5,7 @@ namespace Io.Schnurr.Circles.App.Store.App;
 public static class AppReducer
 {
     [ReducerMethod]
-    public static AppState SetState(AppState state, SetState action) => action.AppState;
+    public static AppState SetState(AppState state, SetState action) => action.state;
 
     [ReducerMethod(typeof(SetDefaultState))]
     public static AppState SetDefaultState(AppState state) => new AppState();
--- Io.Schnurr.Circles.App/Store/Board/BoardReducers.cs
+++ Io.Schnurr.Circles.App/Store/Board/BoardReducers.cs
@@ -5,7 +5,7 @@ namespace Io.Schnurr.Circles.App.Store.Board;
 public static class BoardReducer
 {
     [ReducerMethod]
-    public static BoardState SetState(BoardState state, SetState action) => action.BoardState;
+    public static BoardState SetState(BoardState state, SetState action) => action.state;
 
     [ReducerMethod(typeof(SetDefaultState))]
     public static BoardState SetDefaultState(BoardState state) => new BoardState() with { IsTileView = true };
