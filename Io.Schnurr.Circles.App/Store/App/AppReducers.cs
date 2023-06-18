101143e0ef982bb983d7ef29a632b2b1ff470f6b
Samuel Schnurr <ssn@objektkultur.de>
2023-06-18 10:53:08 +0200

    Add Reducer Info SetState

--- Io.Schnurr.Circles.App/Store/App/AppReducers.cs
+++ Io.Schnurr.Circles.App/Store/App/AppReducers.cs
@@ -4,6 +4,9 @@ namespace Io.Schnurr.Circles.App.Store.App;
 
 public static class AppReducer
 {
+    // Better would be to create separate reducers or effects for each action
+    // This would improve state tracing in DevTools, clean accessing of state transformation methods instead of access in components and more
+    // But for simplification reasons in this prototype I use a single SetState reducer
     [ReducerMethod]
     public static AppState SetState(AppState state, SetStateAction action) => action.state;
 }
--- Io.Schnurr.Circles.App/Store/Board/BoardReducers.cs
+++ Io.Schnurr.Circles.App/Store/Board/BoardReducers.cs
@@ -4,6 +4,9 @@ namespace Io.Schnurr.Circles.App.Store.Board;
 
 public static class BoardReducer
 {
+    // Better would be to create separate reducers or effects for each action
+    // This would improve state tracing in DevTools, clean accessing of state transformation methods instead of access in components and more
+    // But for simplification reasons in this prototype I use a single SetState reducer
     [ReducerMethod]
     public static BoardState SetState(BoardState state, SetStateAction action) => action.state;
 }
