9846bc376629587f7d1c9a52a295def56bdd935c
Samuel Schnurr <ssn@objektkultur.de>
2023-06-20 19:23:14 +0200

    Add PaperSwitchItem

---
+++ Io.Schnurr.Circles.App/Models/PaperSwitchItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Io.Schnurr.Circles.App.Models;
+
+public class PaperSwitchItem
+{
+    public PaperSwitchItem(string label, bool isChecked, bool isDisabled)
+    {
+        this.label = label;
+        this.isChecked = isChecked;
+        this.isDisabled = isDisabled;
+    }
+
+    internal readonly string label;
+    internal bool isChecked;
+    internal bool isDisabled;
+}
