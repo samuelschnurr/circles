f045ec9a8c9bcc48aa8c8b4af9bbfbeaa2170ead
Samuel Schnurr <ssn@objektkultur.de>
2023-06-23 17:06:15 +0200

    Add BoardToolBar to Index Page

--- Io.Schnurr.Circles.App/Pages/Index.razor.cs
+++ Io.Schnurr.Circles.App/Pages/Index.razor.cs
@@ -1,16 +1,10 @@
 ﻿using Io.Schnurr.Circles.App.Interfaces;
-using Io.Schnurr.Circles.App.Store.Board;
 
 namespace Io.Schnurr.Circles.App.Pages;
 
 public partial class Index : ILoadingComponent
 {
     public bool IsLoading() => BoardState.Value.IsTileView == null;
-
-    private void ToggleTileView()
-    {
-        Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { IsTileView = !BoardState.Value.IsTileView }));
-    }
 
     private bool ShowTileView() => BoardState.Value.IsTileView == true;
 }
