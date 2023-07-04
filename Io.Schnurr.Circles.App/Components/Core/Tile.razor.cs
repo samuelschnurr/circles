ca87a8a12d07ff389fc933efd4e02232e8131606
Samuel Schnurr <ssn@objektkultur.de>
2023-07-04 23:45:26 +0200

    Extract Tile as separate Component from AdvertisementTile

---
+++ Io.Schnurr.Circles.App/Components/Core/Tile.razor.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Io.Schnurr.Circles.App.Components.Core;
+
+public partial class Tile
+{
+    [Parameter]
+    public EventCallback HandleClick { get; set; }
+
+    [Parameter]
+    public string Title { get; set; }
+
+    [Parameter]
+    public string Description { get; set; }
+
+    [Parameter]
+    public string SubTitleLeft { get; set; }
+
+    [Parameter]
+    public string SubTitleRight { get; set; }
+
+    [Parameter]
+    public string Image { get; set; }
+}
