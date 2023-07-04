674bd1147a7ef3d01438c34bfcda78eae2166d42
Samuel Schnurr <ssn@objektkultur.de>
2023-07-05 00:55:21 +0200

    Delete NavMenuItem Model and use Tuple

--- Io.Schnurr.Circles.App/Components/Base/NavMenu.razor.cs
+++ Io.Schnurr.Circles.App/Components/Base/NavMenu.razor.cs
@@ -1,14 +1,14 @@
-﻿using Io.Schnurr.Circles.App.Models;
-using Microsoft.AspNetCore.Components.Routing;
+﻿using Microsoft.AspNetCore.Components.Routing;
 using MudBlazor;
 
 namespace Io.Schnurr.Circles.App.Components.Base;
 
 public partial class NavMenu
 {
-    private readonly NavMenuItem[] navMenuItems = new NavMenuItem[] {
-        new NavMenuItem("Board", "", Icons.Material.Filled.Dashboard, NavLinkMatch.All),
-        new NavMenuItem("Pins", "pins", Icons.Material.Filled.PushPin),
-        new NavMenuItem("Circles", "circles", Icons.Material.Filled.BubbleChart)
+    private readonly (string label, string href, string icon, NavLinkMatch match)[] navMenuItems =
+    {
+        ("Board", "", Icons.Material.Filled.Dashboard, NavLinkMatch.All),
+        ("Pins", "pins", Icons.Material.Filled.PushPin, default(NavLinkMatch)),
+        ("Circles", "circles", Icons.Material.Filled.BubbleChart, default(NavLinkMatch))
     };
 }
