b72b980f0a47eccc00e45f45dce150b62d29ebca
Samuel Schnurr <ssn@objektkultur.de>
2023-07-04 23:52:01 +0200

    Move State from Progressbar Core to outside as Parameter

---
+++ Io.Schnurr.Circles.App/Components/Core/ProgressBar.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Io.Schnurr.Circles.App.Components.Core;
+
+public partial class ProgressBar
+{
+    [Parameter]
+    public bool IsDrawerOpen { get; set; }
+}
