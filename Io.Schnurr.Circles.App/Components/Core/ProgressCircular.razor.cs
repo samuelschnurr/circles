11184977e329e27f1ca0439944b23d3e7f7a8d83
Samuel Schnurr <ssn@objektkultur.de>
2023-08-05 11:29:14 +0200

    Rename ProgressBar to ProgressCircular

--- Io.Schnurr.Circles.App/Components/Core/ProgressCircular.razor.cs
+++ Io.Schnurr.Circles.App/Components/Core/ProgressCircular.razor.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace Io.Schnurr.Circles.App.Components.Core;
 
-public partial class ProgressBar
+public partial class ProgressCircular
 {
     [Parameter]
     public bool IsDrawerOpen { get; set; }
