6aa12d8a05eb7b56137f89c5ce3ddd30a839ecb2
Samuel Schnurr <ssn@objektkultur.de>
2023-08-05 20:34:56 +0200

    Added random number to index of testdata to provide better difficulty

--- Io.Schnurr.Circles.Api/Services/AdvertisementService.cs
+++ Io.Schnurr.Circles.Api/Services/AdvertisementService.cs
@@ -2,20 +2,22 @@
 
 namespace Io.Schnurr.Circles.Api.Services;
 
-internal static class AdvertisementService
+internal static class AdvertisementService // TODO: Cleanup
 {
     #region MockDB
+
+    private static Random rnd = new Random();
 
     private static readonly IEnumerable<Advertisement> advertisements = Enumerable.Range(1, 50).Select(index =>
               new Advertisement()
               {
 
                   Id = index,
-                  Title = "Title" + index,
+                  Title = "Title-" + rnd.Next(1, 10000) + "-" + index,
                   Image = new(),
-                  CreatedAt = DateTime.Now.AddDays(-index),
+                  CreatedAt = DateTime.Now.AddDays(-index * rnd.Next(1, 5000)),
                   DeletedAt = index % 4 == 0 ? DateTime.Now.AddMinutes(-index) : null,
-                  Description = "Description Description" + index,
+                  Description = "Description-" + rnd.Next(1, 10000) + "-" + index,
                   Price = 1.3M * index,
               });
 
