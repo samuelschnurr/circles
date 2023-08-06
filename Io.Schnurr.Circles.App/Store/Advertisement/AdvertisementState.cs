27f9e5f6d35338951adfcddee4f1c4281273790f
Samuel Schnurr <ssn@objektkultur.de>
2023-08-06 19:45:25 +0200

    Made   public IEnumerable<Shared.Models.Advertisement>? Items nullable

--- Io.Schnurr.Circles.App/Store/Advertisement/AdvertisementState.cs
+++ Io.Schnurr.Circles.App/Store/Advertisement/AdvertisementState.cs
@@ -5,7 +5,7 @@ namespace Io.Schnurr.Circles.App.Store.Advertisement;
 [FeatureState]
 public record AdvertisementState
 {
-    public IEnumerable<Shared.Models.Advertisement>? Items { get; init; }
+    public IEnumerable<Shared.Models.Advertisement>? Items { get; init; } = null;
 
     public AdvertisementState() { }
 }
