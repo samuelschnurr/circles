e526d212a79358981776977535538772467e4849
Samuel Schnurr <ssn@objektkultur.de>
2023-08-06 19:40:01 +0200

    Cleanup AdvertisementToolBar

---
+++ Io.Schnurr.Circles.App/Components/Views/Advertisements/AdvertisementToolBar.razor.cs
@@ -0,0 +1,28 @@
+﻿using Fluxor;
+using Io.Schnurr.Circles.App.Store.Board;
+using Io.Schnurr.Circles.Shared.Enums;
+using Microsoft.AspNetCore.Components;
+
+namespace Io.Schnurr.Circles.App.Components.Views.Advertisements;
+
+public partial class AdvertisementToolBar
+{
+    [Inject]
+    private IState<BoardState> BoardState { get; set; }
+
+    private SortColumn SortColumn => BoardState.Value.SortColumn;
+
+    private SortDirection SortDirection => BoardState.Value.SortDirection;
+
+    private string SearchString => BoardState.Value.SearchString;
+
+    private bool ShowTileView => BoardState.Value.IsTileView == true;
+
+    private void ToggleTileView() => Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { IsTileView = !BoardState.Value.IsTileView }));
+
+    private void OrderBySortColumn(SortColumn column) => Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { SortColumn = column }));
+
+    private void OrderBySortDirection(SortDirection direction) => Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { SortDirection = direction }));
+
+    private void SearchByString(string search) => Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { SearchString = search }));
+}
