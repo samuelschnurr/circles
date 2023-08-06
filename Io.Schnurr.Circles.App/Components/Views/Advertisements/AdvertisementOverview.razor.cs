91a70186e9a11a5ba8031a9225216670797ae5e9
Samuel Schnurr <ssn@objektkultur.de>
2023-08-06 18:51:47 +0200

    Render AdvertisementOverview state correctly

--- Io.Schnurr.Circles.App/Components/Views/Advertisements/AdvertisementOverview.razor.cs
+++ Io.Schnurr.Circles.App/Components/Views/Advertisements/AdvertisementOverview.razor.cs
@@ -1,18 +1,29 @@
-﻿using Io.Schnurr.Circles.Shared.Models;
+﻿using Fluxor;
+using Io.Schnurr.Circles.App.Store.Advertisement;
+using Io.Schnurr.Circles.App.Store.App;
+using Io.Schnurr.Circles.App.Store.Board;
+using Io.Schnurr.Circles.App.Utils;
+using Io.Schnurr.Circles.Shared.Models;
 using Microsoft.AspNetCore.Components;
 
 namespace Io.Schnurr.Circles.App.Components.Views.Advertisements;
 
 public partial class AdvertisementOverview
 {
-    [Parameter]
-    public IEnumerable<Advertisement> Data { get; init; }
+    [Inject]
+    private IState<AdvertisementState> AdvertisementState { get; set; }
 
-    [Parameter]
-    public bool IsDrawerOpen { get; set; }
+    [Inject]
+    private IState<AppState> AppState { get; set; }
 
-    [Parameter]
-    public bool ShowTileView { get; set; }
+    [Inject]
+    private IState<BoardState> BoardState { get; set; }
 
-    public bool IsLoading => Data == null;
+    private IEnumerable<Advertisement>? Data => AdvertisementState.Value.Items;
+
+    public bool IsDrawerOpen => AppState.Value.IsDrawerOpen;
+
+    public bool? ShowTileView => BoardState.Value.IsTileView;
+
+    public bool IsLoading => Helpers.HasNull(Data, ShowTileView);
 }
