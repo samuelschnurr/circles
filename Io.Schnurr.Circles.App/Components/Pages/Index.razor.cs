48df58c10c024739089fc8102bd3ce7b186d3021
Samuel Schnurr <ssn@objektkultur.de>
2023-08-06 19:10:18 +0200

    Extracted logic fdrom index page to setup separate advertisementtoolbar componentview

--- Io.Schnurr.Circles.App/Components/Pages/Index.razor.cs
+++ Io.Schnurr.Circles.App/Components/Pages/Index.razor.cs
@@ -1,7 +1,6 @@
-﻿using Io.Schnurr.Circles.App.Services;
+﻿using Fluxor;
+using Io.Schnurr.Circles.App.Store.App;
 using Io.Schnurr.Circles.App.Store.Board;
-using Io.Schnurr.Circles.Shared.Enums;
-using Io.Schnurr.Circles.Shared.Models;
 using Microsoft.AspNetCore.Components;
 
 namespace Io.Schnurr.Circles.App.Components.Pages;
@@ -9,55 +8,12 @@ namespace Io.Schnurr.Circles.App.Components.Pages;
 public partial class Index
 {
     [Inject]
-    private AdvertisementService AdvertisementService { get; init; }
+    private IState<AppState> AppState { get; set; }
 
-    private IEnumerable<Advertisement>? advertisements;
+    [Inject]
+    private IState<BoardState> BoardState { get; set; }
 
-    private SortColumn SortColumn => BoardState.Value.SortColumn;
-
-    private SortDirection SortDirection => BoardState.Value.SortDirection;
-
-    public string SearchString => BoardState.Value.SearchString;
+    public bool IsDrawerOpen => AppState.Value.IsDrawerOpen;
 
     public bool IsLoading => BoardState.Value.IsTileView == null;
-
-    private bool ShowTileView => BoardState.Value.IsTileView == true;
-
-    protected override async Task OnInitializedAsync()
-    {
-        await LoadAdvertisements();
-    }
-
-    private async Task LoadAdvertisements()
-    {
-        advertisements = null;
-        var searchString = BoardState.Value.SearchString;
-        var data = await AdvertisementService.GetAll(true, searchString);
-        advertisements = AdvertisementService.SortAdvertisements(data, SortColumn, SortDirection);
-    }
-
-    private void SortAdvertisements()
-    {
-        advertisements = AdvertisementService.SortAdvertisements(advertisements, SortColumn, SortDirection);
-    }
-
-    private void ToggleTileView()
-    {
-        Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { IsTileView = !BoardState.Value.IsTileView }));
-    }
-
-    private void OrderBySortColumn(SortColumn column)
-    {
-        Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { SortColumn = column }, EventCallback.Factory.Create(this, SortAdvertisements)));
-    }
-
-    private void OrderBySortDirection(SortDirection direction)
-    {
-        Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { SortDirection = direction }, EventCallback.Factory.Create(this, SortAdvertisements)));
-    }
-
-    private void SearchByString(string search)
-    {
-        Dispatcher.Dispatch(new UpdateStateAction(BoardState.Value with { SearchString = search }, EventCallback.Factory.Create(this, LoadAdvertisements)));
-    }
 }
