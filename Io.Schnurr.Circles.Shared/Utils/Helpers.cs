9b79737f482c67541c87d38063f6f065a0cc0bca
Samuel Schnurr <ssn@objektkultur.de>
2023-08-06 18:45:18 +0200

    Added Helper to check if a object in params is null to support IsLoading easier

---
+++ Io.Schnurr.Circles.Shared/Utils/Helpers.cs
@@ -0,0 +1,16 @@
+﻿namespace Io.Schnurr.Circles.App.Utils;
+
+public static class Helpers
+{
+    public static bool HasNull(params dynamic?[] nullableValues)
+    {
+        foreach (var value in nullableValues)
+        {
+            if (value is null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
