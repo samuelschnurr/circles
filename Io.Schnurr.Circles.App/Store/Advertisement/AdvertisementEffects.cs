fd7b3debe1af454dc15ef42f0740946c44dedf72
Samuel Schnurr <ssn@objektkultur.de>
2023-08-07 19:13:14 +0200

    Inject AdvertisementService in AdvertisementEffect

--- Io.Schnurr.Circles.App/Store/Advertisement/AdvertisementEffects.cs
+++ Io.Schnurr.Circles.App/Store/Advertisement/AdvertisementEffects.cs
@@ -8,7 +8,7 @@ public class AdvertisementEffects
     private readonly ILocalStorageService localStorageService;
     private const string persistanceName = "circles-advertisement";
 
-    public AdvertisementEffects(ILocalStorageService localStorageService)
+    public AdvertisementEffects(ILocalStorageService localStorageService, AdvertisementService advertisementService)
     {
         this.localStorageService = localStorageService;
     }
