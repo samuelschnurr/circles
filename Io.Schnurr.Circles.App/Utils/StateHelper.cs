d507567e790ed7e11fd351d98d9926ef4b3b5d15
Samuel Schnurr <ssn@objektkultur.de>
2023-08-08 17:57:31 +0200

    Add StateHelper wit PersistState and PersistAction

---
+++ Io.Schnurr.Circles.App/Utils/StateHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Io.Schnurr.Circles.App.Utils;
+
+/// <summary>
+/// If a state derives from this record it will be able to be persisted to local storage.
+/// </summary>
+public abstract record PersistableState
+{
+    internal abstract string PersistanceName { get; }
+}
+
+/// <summary>
+/// If a action derives from this record it will persist the new state in local storage, if the state itself is persistable. 
+/// </summary>
+public abstract record PersistAfterDispatchAction<T>(T state);
