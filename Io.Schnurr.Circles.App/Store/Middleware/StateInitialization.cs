b5e2c4f50f9f696caf2d824e8450d378aed65cc4
Samuel Schnurr <ssn@objektkultur.de>
2023-08-10 14:20:58 +0200

    Dynamically initialize states with InitializeOnStartupAttribute

--- Io.Schnurr.Circles.App/Store/Middleware/StateInitialization.cs
+++ Io.Schnurr.Circles.App/Store/Middleware/StateInitialization.cs
@@ -1,17 +1,24 @@
-﻿using Fluxor;
+﻿using System.Reflection;
+using Fluxor;
+using Io.Schnurr.Circles.App.Utils;
 
 namespace Io.Schnurr.Circles.App.Store.Middleware;
 
 /// <summary>
-/// Initialize the Fluxor State on initialize by default values or by localStorage.
+/// Executes all FluxorStateActions which contain the <see cref="InitializeOnStartupAttribute"/>.
 /// </summary>
 public class StateInitialization : Fluxor.Middleware
 {
     public override Task InitializeAsync(IDispatcher dispatcher, IStore store)
     {
-        dispatcher.Dispatch(new App.InitializeStateAction());
-        dispatcher.Dispatch(new Board.InitializeStateAction());
-        dispatcher.Dispatch(new Advertisement.InitializeStateAction());
+        var assembly = Assembly.GetExecutingAssembly();
+        var methodsWithAttribute = assembly.GetTypes().Where(t => t.GetCustomAttribute<InitializeOnStartupAttribute>() != null);
+
+        foreach (var method in methodsWithAttribute)
+        {
+            var initializationMethod = Activator.CreateInstance(method);
+            dispatcher.Dispatch(initializationMethod);
+        }
 
         return Task.CompletedTask;
     }
