edaffa0ca128c960de5e6b4af2f43cf773b9b809
Samuel Schnurr <ssn@objektkultur.de>
2023-08-25 15:01:10 +0200

    Add route class

---
+++ Io.Schnurr.Circles.App/Utils/Routes.cs
@@ -0,0 +1,25 @@
+﻿namespace Io.Schnurr.Circles.App.Utils;
+
+internal abstract class Routes
+{
+    internal abstract class Index
+    {
+        internal const string Base = "/";
+    }
+
+    internal abstract class Circles
+    {
+        internal const string Base = "/circles";
+    }
+
+    internal abstract class Pins
+    {
+        internal const string Base = "/pins";
+    }
+
+    internal abstract class Advertisement
+    {
+        internal const string Base = "/advertisement";
+        internal const string Details = $"{Base}/{{Id:int}}";
+    }
+}
