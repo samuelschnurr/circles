b6030f8b728387b1efaff07254137a079b40ab90
Samuel Schnurr <ssn@objektkultur.de>
2023-08-25 17:47:04 +0200

    Cleaned up board views

--- Io.Schnurr.Circles.App/Features/Board/Views/BoardIndex.razor.cs
+++ Io.Schnurr.Circles.App/Features/Board/Views/BoardIndex.razor.cs
@@ -1,9 +1,6 @@
 ﻿using Fluxor;
 using Io.Schnurr.Circles.App.Features.App.Store;
 using Io.Schnurr.Circles.App.Features.Board.Store;
-using Io.Schnurr.Circles.App.Services;
-using Io.Schnurr.Circles.App.Utils;
-using Io.Schnurr.Circles.Shared.Models;
 using Microsoft.AspNetCore.Components;
 
 namespace Io.Schnurr.Circles.App.Features.Board.Views;
@@ -16,30 +13,7 @@ public partial class BoardIndex
     [Inject]
     private IState<BoardState> BoardState { get; set; }
 
-    [Inject]
-    private NavigationManager NavigationManager { get; set; }
-
-    private IEnumerable<Advertisement> Data => GetAdvertisements();
-
     public bool IsDrawerOpen => AppState.Value.IsDrawerOpen;
 
-    public bool ShowTileView => BoardState.Value.IsTileView;
-
-    public bool ShowLoadingSpinner => !BoardState.Value.IsInitialized || BoardState.Value.IsLoading;
-
-    protected override async Task OnInitializedAsync()
-    {
-        Dispatcher.Dispatch(new LoadAdvertisementsAction());
-        await Task.CompletedTask;
-    }
-
-    private IEnumerable<Advertisement> GetAdvertisements()
-    {
-        var advertisements = BoardState.Value.Items;
-        var filteredAdvertisements = AdvertisementService.FilterAdvertisements(advertisements, BoardState.Value.SearchString);
-        var filteredAndSortedAdvertisements = AdvertisementService.SortAdvertisements(filteredAdvertisements, BoardState.Value.SortColumn, BoardState.Value.SortDirection);
-        return filteredAndSortedAdvertisements;
-    }
-
-    private void NavigateToAdvertisementDetail(int id) => NavigationManager.NavigateTo(Routes.Board.GetDetailPath(id));
+    public bool ShowLoadingSpinner => !BoardState.Value.IsInitialized;
 }
