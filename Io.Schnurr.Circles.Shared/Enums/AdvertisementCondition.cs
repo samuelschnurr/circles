9b621335b448737ba501eeff4aa24bac9660fb99
Samuel Schnurr <ssn@objektkultur.de>
2023-09-08 11:45:02 +0200

    Add two createdby and condition to advertisement

---
+++ Io.Schnurr.Circles.Shared/Enums/AdvertisementCondition.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Io.Schnurr.Circles.Shared.Enums;
+
+public enum AdvertisementCondition
+{
+    New,
+    [Display(Name = "Very Good")]
+    VeryGood,
+    Good,
+    Acceptable
+}
--- Io.Schnurr.Circles.Shared/Models/Advertisement.cs
+++ Io.Schnurr.Circles.Shared/Models/Advertisement.cs
@@ -1,9 +1,13 @@
-﻿namespace Io.Schnurr.Circles.Shared.Models;
+﻿using Io.Schnurr.Circles.Shared.Enums;
+
+namespace Io.Schnurr.Circles.Shared.Models;
 
 public class Advertisement : Base
 {
     public string Title { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
+    public string CreatedBy { get; set; }
+    public AdvertisementCondition Condition { get; set; }
     public File Image { get; set; }
 }
