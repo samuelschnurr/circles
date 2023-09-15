9e9e63ae4558965e71f6a1143ac938184f09571e
Samuel Schnurr <ssn@objektkultur.de>
2023-09-15 19:14:13 +0200

    Add OfferDetail skeleton

---
+++ Io.Schnurr.Circles.App/Features/Offer/Views/OfferDetail.razor.cs
@@ -0,0 +1,36 @@
+using Fluxor;
+using Io.Schnurr.Circles.App.Features.Offer.Store;
+using Io.Schnurr.Circles.App.Utils;
+using Io.Schnurr.Circles.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using static Io.Schnurr.Circles.App.Features.Offer.Store.OfferActions;
+
+namespace Io.Schnurr.Circles.App.Features.Offer.Views;
+
+public partial class OfferDetail
+{
+    [Parameter]
+    public int Id { get; set; }
+
+    [Inject]
+    private IState<OfferState> OfferState { get; set; }
+
+    [Inject]
+    NavigationManager NavigationManager { get; set; }
+
+    private bool ShowLoadingSpinner => Advertisement == null;
+
+    private Advertisement? Advertisement => OfferState.Value.Items?.Single(i => i.Id == Id);
+
+    protected override Task OnInitializedAsync()
+    {
+        if (OfferState.Value.Items == null)
+        {
+            Dispatcher.Dispatch(new LoadAdvertisementsAction());
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private void NavigateBack() => NavigationManager.NavigateTo(Routes.Offer.Index);
+}
