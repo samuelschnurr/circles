9e863450a2b0664bfb9581a30abfafd4acf13b06
Samuel Schnurr <ssn@objektkultur.de>
2023-09-16 13:08:07 +0200

    Remove IsDrawerOpen from ProgressCircular to center always in view context

--- Io.Schnurr.Circles.App/Components/Core/ProgressCircular.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace Io.Schnurr.Circles.App.Components.Core;
-
-public partial class ProgressCircular
-{
-    [Parameter]
-    public bool IsDrawerOpen { get; set; }
-}
