ade06cde9a3dfab437a882df3e2b0926476571b2
Samuel Schnurr <ssn@objektkultur.de>
2023-09-21 21:03:47 +0200

    Add BaseValidator

---
+++ Io.Schnurr.Circles.Shared/Validators/BaseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Io.Schnurr.Circles.Shared.Validators;
+
+internal class BaseValidator<T> : AbstractValidator<T>
+{
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<T>.CreateWithOptions((T)model, x => x.IncludeProperties(propertyName)));
+
+        if (result.IsValid)
+        {
+            return Array.Empty<string>();
+        }
+
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
