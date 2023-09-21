cf35be4366c498385c6fbaae8ad34de176e846ba
Samuel Schnurr <ssn@objektkultur.de>
2023-09-21 21:12:02 +0200

    Add AdvertisementValidator

---
+++ Io.Schnurr.Circles.Shared/Validators/AdvertisementValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Io.Schnurr.Circles.Shared.Models;
+
+namespace Io.Schnurr.Circles.Shared.Validators;
+
+
+internal class AdvertisementValidator : BaseValidator<Advertisement>
+{
+    public AdvertisementValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .Length(10, 50);
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .Length(100, 10000);
+
+        RuleFor(x => x.Price)
+            .NotEmpty()
+            .InclusiveBetween(0, 999999.99M)
+            .PrecisionScale(2, 2, true);
+
+        RuleFor(x => x.Condition)
+            .IsInEnum();
+
+        RuleFor(x => x.Base64Image)
+            .NotEmpty();
+    }
+}
