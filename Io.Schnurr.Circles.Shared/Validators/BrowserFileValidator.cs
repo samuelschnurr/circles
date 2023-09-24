e58c73e57e3e57238f54203452733a03af9b9659
Samuel Schnurr <ssn@objektkultur.de>
2023-09-24 16:31:02 +0200

    Add BrowserFileValidator

---
+++ Io.Schnurr.Circles.Shared/Validators/BrowserFileValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Io.Schnurr.Circles.Shared.Models;
+
+namespace Io.Schnurr.Circles.Shared.Validators;
+
+
+public class BrowserFileValidator : BaseValidator<BrowserFile>
+{
+    public BrowserFileValidator()
+    {
+        RuleFor(x => x.File)
+            .NotEmpty();
+
+        When(x => x.File != null, () =>
+        {
+            RuleFor(x => x.File.Name)
+              .NotEmpty();
+
+            RuleFor(x => x.File.Size)
+            .LessThanOrEqualTo(10485760)
+            .WithMessage("The maximum file size is 10 MB");
+        });
+    }
+}
