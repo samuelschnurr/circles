5aa67cc6bb78bb5322ae5eb9a09172ee7acd7943
Samuel Schnurr <ssn@objektkultur.de>
2023-09-26 18:44:39 +0200

    Add ErrorBoundary

--- Io.Schnurr.Circles.App/Components/Base/MainLayout.razor.cs
+++ Io.Schnurr.Circles.App/Components/Base/MainLayout.razor.cs
@@ -1,15 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
 using static Io.Schnurr.Circles.App.Features.App.Store.AppActions;
 
 namespace Io.Schnurr.Circles.App.Components.Base;
 
 public sealed partial class MainLayout : IDisposable
 {
+    private ErrorBoundary ErrorBoundary { get; set; }
+
     protected override void OnInitialized()
     {
         // MainLayout cant inherit vom FluxorComponent to rerender automatically on StateChanges beause it inherits vom LayoutComponentBase
         // So manually call the components StateHasChanged, whenever the state of AppState has changed.
         AppState.StateChanged += HandleStateHasChanged;
     }
+
+    protected override void OnParametersSet() => RecoverError();
 
     private void ToggleDrawerViaAppBar() => Dispatcher.Dispatch(new ToggleDrawerViaAppBarAction());
 
@@ -19,8 +24,7 @@ public sealed partial class MainLayout : IDisposable
 
     private void HandleStateHasChanged(object? sender, EventArgs e) => InvokeAsync(StateHasChanged);
 
-    public void Dispose()
-    {
-        AppState.StateChanged -= HandleStateHasChanged;
-    }
+    public void Dispose() => AppState.StateChanged -= HandleStateHasChanged;
+
+    private void RecoverError() => ErrorBoundary.Recover();
 }
