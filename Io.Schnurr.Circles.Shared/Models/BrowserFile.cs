5ba191d55fd045eba721a1b595ea2d01c0873dc8
Samuel Schnurr <ssn@objektkultur.de>
2023-09-27 15:35:30 +0200

    Add [JsonIgnore] to BrowserFile to not send it to the api

--- Io.Schnurr.Circles.Shared/Models/BrowserFile.cs
+++ Io.Schnurr.Circles.Shared/Models/BrowserFile.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Io.Schnurr.Circles.Shared.Models;
 
 public class BrowserFile
 {
+    [JsonIgnore]
     public IBrowserFile File { get; set; }
+
     public string Base64File { get; set; }
 }
