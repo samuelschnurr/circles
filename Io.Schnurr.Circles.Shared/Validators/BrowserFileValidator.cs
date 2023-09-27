ecb465da388a502587533835fbd943a058451bb3
Samuel Schnurr <ssn@objektkultur.de>
2023-09-27 21:26:10 +0200

    Add Mimetype validation

--- Io.Schnurr.Circles.Shared/Validators/BrowserFileValidator.cs
+++ Io.Schnurr.Circles.Shared/Validators/BrowserFileValidator.cs
@@ -16,7 +16,17 @@ public class BrowserFileValidator : BaseValidator<BrowserFile>
         .LessThanOrEqualTo(10485760)
         .WithMessage("The maximum file size is 10 MB");
 
+        RuleFor(x => x.ContentType)
+             .Must(BeValidImageType)
+             .WithMessage("Invalid file type. Please select JPG, JPEG, or PNG files.");
+
         RuleFor(x => x.Base64File)
           .NotEmpty();
+    }
+
+    private bool BeValidImageType(string contentType)
+    {
+        var validMimeTypes = new[] { "image/jpeg", "image/png" };
+        return validMimeTypes.Contains(contentType);
     }
 }
