643671a4a425378e2d6f11a10932a789cfea57dd
Samuel Schnurr <ssn@objektkultur.de>
2023-09-30 13:30:51 +0200

    Remove Interceptor and Add HttpStatusCodeService

---
+++ Io.Schnurr.Circles.App/Services/HttpStatusCodeService.cs
@@ -0,0 +1,29 @@
+﻿using MudBlazor;
+
+namespace Io.Schnurr.Circles.App.Services;
+
+internal class HttpStatusCodeService : DelegatingHandler
+{
+    private readonly ISnackbar snackbar;
+
+    public HttpStatusCodeService(ISnackbar snackbar)
+    {
+        this.snackbar = snackbar;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var response = await base.SendAsync(request, cancellationToken);
+
+        if (response.IsSuccessStatusCode)
+        {
+            snackbar.Add("Success!", Severity.Success);
+        }
+        else
+        {
+            snackbar.Add("Error!", Severity.Error);
+        }
+
+        return response;
+    }
+}
